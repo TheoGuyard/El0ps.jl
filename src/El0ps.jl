68eae96fbfd166609a11b18ec905f95c7bbe8807
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-11-08 14:47:34 +0100

    Fix tests and remove CI with Julia v1.5

--- src/El0ps.jl
+++ src/El0ps.jl
@@ -26,8 +26,8 @@ include("penalties/bigm.jl")
 include("penalties/l1norm.jl")
 include("penalties/l2norm.jl")
 include("penalties/l1l2norm.jl")
-include("penalties/BigmL1norm.jl")
-include("penalties/BigmL2norm.jl")
+include("penalties/bigml1norm.jl")
+include("penalties/bigml2norm.jl")
 export Bigm, L1norm, L2norm, L1L2norm, BigmL1norm, BigmL2norm
 
 include("problem.jl")

2e0fcecec447838f2551019f7190b2c0a283e095
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-11-23 10:16:43 +0100

    Remove 1/2 factor in from on L2-norms (#15)

--- src/penalties/bigml2norm.jl
+++ src/penalties/bigml2norm.jl
@@ -1,7 +1,7 @@
 """
     BigmL2norm
 
-Big-M plus L2-norm function G(x) = (α/2) * ||x||_2^2 + Ind(||x||_Inf <= M).
+Big-M plus L2-norm function G(x) = α * ||x||_2^2 + Ind(||x||_Inf <= M).
 
 # Arguments
 
@@ -16,19 +16,19 @@ struct BigmL2norm <: AbstractPenalty
     function BigmL2norm(M::Float64, α::Float64)
         (M > 0.) || error("Parameter M must be positive")
         (α > 0.) || error("Parameter α must be positive")
-        τ = sqrt(2. / α) < M ? sqrt(2. * α) : (1. / M) + (α / 2.) * M
-        μ = sqrt(2. / α) < M ? τ / α : M
+        τ = sqrt(1. / α) < M ? sqrt(4. * α) : (1. / M) + α * M
+        μ = sqrt(1. / α) < M ? τ / (2. * α) : M
         return new(M, α, τ, μ)
     end
 end
 
 Base.show(io::IO, G::BigmL2norm) = print(io, "Bigm + L2-norm")
-value_1d(G::BigmL2norm, x::Float64) = abs(x) <= G.M ? (G.α / 2.) * x^2 : Inf
+value_1d(G::BigmL2norm, x::Float64) = abs(x) <= G.M ? G.α * x^2 : Inf
 function conjugate_1d(G::BigmL2norm, v::Float64)
-    r = clamp(v / G.α, -G.M, G.M)
-    return v * r - (G.α / 2.) * r^2
+    r = clamp(v / (2. * G.α), -G.M, G.M)
+    return v * r - G.α * r^2
 end
-prox_1d(G::BigmL2norm, x::Float64, η::Float64) = clamp(x / (1. + η * G.α), -G.M, G.M)
+prox_1d(G::BigmL2norm, x::Float64, η::Float64) = clamp(x / (1. + 2. * η * G.α), -G.M, G.M)
 dual_scale!(G::BigmL2norm, A::Matrix, u::Vector, λ::Float64) = A' * u
 
 function bind_model!(G::BigmL2norm, model::JuMP.Model)
@@ -42,6 +42,6 @@ function bind_model!(G::BigmL2norm, model::JuMP.Model)
     end
     @constraint(model, model[:x] .>= -G.M .* model[:z])
     @constraint(model, model[:x] .<= G.M .* model[:z])
-    @constraint(model, model[:Ωcost] >= sum(model[:z]) + (G.α / 2.) * sum(s))
+    @constraint(model, model[:Ωcost] >= sum(model[:z]) + G.α * sum(s))
     return nothing
 end
--- src/penalties/l2norm.jl
+++ src/penalties/l2norm.jl
@@ -1,7 +1,7 @@
 """
     L2norm
 
-L2-norm function G(x) = (α/2) * ||x||_2^2.
+L2-norm function G(x) = α * ||x||_2^2.
 
 # Arguments
 
@@ -13,16 +13,16 @@ struct L2norm <: AbstractPenalty
     μ::Float64
     function L2norm(α::Float64)
         (α > 0.) || error("Parameter α must be positive")
-        τ = 2. * sqrt(α / 2.)
-        μ = 1. / sqrt(α / 2.)
+        τ = 2. * sqrt(α)
+        μ = 1. / sqrt(α)
         return new(α, τ, μ)
     end
 end
 
 Base.show(io::IO, G::L2norm) = print(io, "L2-norm")
-value_1d(G::L2norm, x::Float64) = (G.α / 2.) * x^2
-conjugate_1d(G::L2norm, v::Float64) = v^2 / (2. * G.α)
-prox_1d(G::L2norm, x::Float64, η::Float64) = x / (1. + η * G.α)
+value_1d(G::L2norm, x::Float64) = G.α * x^2
+conjugate_1d(G::L2norm, v::Float64) = v^2 / (4. * G.α)
+prox_1d(G::L2norm, x::Float64, η::Float64) = x / (1. + 2. * η * G.α)
 dual_scale!(G::L2norm, A::Matrix, u::Vector, λ::Float64) = A' * u
 
 function bind_model!(G::L2norm, model::JuMP.Model)
@@ -34,6 +34,6 @@ function bind_model!(G::L2norm, model::JuMP.Model)
             [0.5 * s[i]; model[:z][i]; model[:x][i]] in RotatedSecondOrderCone()
         )
     end
-    @constraint(model, model[:Ωcost] >= sum(model[:z]) + (G.α / 2.) *  sum(s))
+    @constraint(model, model[:Ωcost] >= sum(model[:z]) + G.α *  sum(s))
     return nothing
 end

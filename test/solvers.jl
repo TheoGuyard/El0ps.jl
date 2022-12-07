b1731026a6e08e9fb38b93559e89027794c001df
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-12-07 15:30:07 +0100

    Add possibility to force initial S0 and S1 (#18)

--- test/solvers.jl
+++ test/solvers.jl
@@ -4,7 +4,7 @@
     n = 30
     ρ = 0.1
     σ = 10.
-    xtrue, A, y = synthetic_data_regression(k, m, n, ρ, σ, normalize=true)
+    x, A, y = synthetic_data_regression(k, m, n, ρ, σ, normalize=true)
     F = LeastSquares()
     G = Bigm(1.)
     λ = 0.1 * compute_λmax(F, G, A, y)
@@ -40,10 +40,39 @@
         @test result.termination_status == MOI.OPTIMAL
     end
 
+    @testset "BnbSolver with initial S0 and S1" begin
+        solver = BnbSolver(
+            verbosity   = false, 
+            maxtime     = 60., 
+            dualpruning = true,
+            l0screening = true,
+            l1screening = true,
+        )
+        S0 = findall(x .== 0.)
+        S1 = findall(x .!= 0.)
+        result = optimize(solver, problem, S0=S0, S1=S1)
+        @test result.node_count == 1
+        @test result.termination_status == MOI.OPTIMAL
+        @test all(result.x[S0] .== 0.)
+        @test all(result.x[S1] .!= 0.)
+    end
+
     @testset "DirectSolver" begin
         options = Dict("display/verblevel" => 0, "limits/gap" => 1e-4)
         solver = DirectSolver(SCIP.Optimizer, options=options)
         result = optimize(solver, problem)
         @test result.termination_status == MOI.OPTIMAL
+    end
+
+    @testset "DirectSolver with initial S0 and S1" begin
+        options = Dict("display/verblevel" => 0, "limits/gap" => 1e-4)
+        solver = DirectSolver(SCIP.Optimizer, options=options)
+        S0 = findall(x .== 0.)
+        S1 = findall(x .!= 0.)
+        result = optimize(solver, problem, S0=S0, S1=S1)
+        @test result.node_count == 1
+        @test result.termination_status == MOI.OPTIMAL
+        @test all(result.x[S0] .== 0.)
+        @test all(result.x[S1] .!= 0.)
     end
 end

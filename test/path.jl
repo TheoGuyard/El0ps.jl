89dee22d79c8d25578842c2fb22a762297d9042d
Theo Guyard <theo.guyard@insa-rennes.fr>
2023-02-27 09:52:31 +0100

    MIT -> AGLP3 license and lint test and doc (#34)

--- test/path.jl
+++ test/path.jl
@@ -1,24 +1,28 @@
 @testset "Path" begin
-    
+
     k = 2
     m = 30
     n = 50
     x = zeros(n)
-    x[1:k] .= 1.
+    x[1:k] .= 1.0
     A = randn(m, n)
     w = A * x
-    y = w + (0.01 * norm(w)) * randn(m) 
+    y = w + (0.01 * norm(w)) * randn(m)
     f = El0ps.LeastSquares(y)
-    h = El0ps.Bigm(1.)
+    h = El0ps.Bigm(1.0)
 
     @testset "Path with BnbSolver" begin
-        solver = El0ps.BnbSolver(verbosity=false, maxtime=60.)
-        path   = El0ps.fit_path(solver, f, h, A, 
-            λratio_min          = 1e-2, 
-            verbosity           = true,
-            max_support_size    = k + 1,
-            stop_if_unsolved    = true,
-            compute_cv          = true,
+        solver = El0ps.BnbSolver(verbosity = false, maxtime = 60.0)
+        path = El0ps.fit_path(
+            solver,
+            f,
+            h,
+            A,
+            λratio_min = 1e-2,
+            verbosity = true,
+            max_support_size = k + 1,
+            stop_if_unsolved = true,
+            compute_cv = true,
         )
         @test all(path.converged .== true)
     end
--- test/problem.jl
+++ test/problem.jl
@@ -2,12 +2,12 @@
     m = 10
     n = 30
     f = El0ps.LeastSquares(randn(m))
-    h = El0ps.Bigm(1.)
+    h = El0ps.Bigm(1.0)
     A = randn(m, n)
     λ = 0.1 * El0ps.compute_λmax(f, h, A)
     problem = El0ps.Problem(f, h, A, λ)
     @test isa(println(problem), Nothing)
     @test all(problem.a .≈ [norm(ai)^2 for ai in eachcol(A)])
-    @test_throws AssertionError El0ps.Problem(f, h, A, -1.)
+    @test_throws AssertionError El0ps.Problem(f, h, A, -1.0)
     @test_throws AssertionError El0ps.Problem(El0ps.LeastSquares(randn(n)), h, A, λ)
 end
